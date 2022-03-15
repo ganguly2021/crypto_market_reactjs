c23b81335bb5681c16233fba11c965f057bd808e
ganguly2021 <sandeepganguly21@gmail.com>
2022-03-15 22:02:09 +0530

    redux toolkit createApi setup.

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,10 +1,22 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
+
 import env from "./../utils/config";
 
-const options = {
-  method: "GET",
-  url: "https://coinranking1.p.rapidapi.com/coins",
-  headers: {
-    "x-rapidapi-host": env.host,
-    "x-rapidapi-key": env.apiKey,
-  },
+// API Headers for Rapid API
+const headers = {
+  "x-rapidapi-host": env.host,
+  "x-rapidapi-key": env.apiKey,
 };
+
+// create api url with HTTP headers
+const createUrl = (url) => ({ url, headers });
+
+export const cryptoApi = createApi({
+  reducerPath: "cryptoApi",
+  baseQuery: fetchBaseQuery({ baseUrl: env.apiBaseUrl }),
+  endpoints: (builder) => ({
+    getCryptos: builder.query({
+      query: () => createUrl("/exchanges"),
+    }),
+  }),
+});
